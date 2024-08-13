8989a206a740c5388ca15d6926740230fd238b69
Mercedes Flores <pikapikagow@gmail.com>
2024-08-13 13:41:02 -0600

    recorrido agregado

---
+++ src/recorrido/app-files/data.js
@@ -0,0 +1,70 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-coesicydet",
+      "name": "COESICYDET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.403099545425449,
+          "pitch": 0.19137034520536922,
+          "rotation": 0,
+          "target": "0-coesicydet"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0996434481248443,
+          "pitch": -0.18693242678633482,
+          "title": "A",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.107573827278351,
+          "pitch": 0.11101700683110494,
+          "title": "A<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.1808621253175513,
+          "pitch": 1.2512684903391715,
+          "title": "A",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.30533266902441447,
+          "pitch": -0.02197604981482293,
+          "title": "A",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
