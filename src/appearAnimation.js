d93afe5515b7853f2f49ccc072a79609c4da300b
Mercedes Flores <pikapikagow@gmail.com>
2024-08-22 10:11:53 -0600

    imagenes

--- src/appearAnimation.js
+++ src/appearAnimation.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const section = document.querySelector("#mi-seccion");
-  
-  const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              section.classList.remove("opacity-0");
-              section.classList.add("animate-fade-up", "animate-delay-[4ms]");
-              observer.unobserve(section); // Dejar de observar después de la animación
-          }
-      });
-  });
+    const section = document.querySelector("#mi-seccion");
 
-  observer.observe(section);
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                section.classList.remove("opacity-0");
+                section.classList.add("animate-fade-up", "animate-delay-[10ms]");
+                observer.unobserve(section); // Dejar de observar después de la animación
+            }
+        });
+    });
+
+    observer.observe(section);
 });
